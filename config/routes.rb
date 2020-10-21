a5ab3e82e31db576e68eb6036314f69be5f9a651
jaroslaw <jaroslaw.chaikin@gmail.com>
2020-10-21 23:59:42 +0300

    Added - About Layout the Promo page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'about', to: 'about#index'
   get 'promo', to: 'promo#index'
   resources :posts
   resources :posts do
     resources :comments
   end
+  resources :subscribers
   get 'posts/index'
 
   root 'posts#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_151520) do
+ActiveRecord::Schema.define(version: 2020_10_21_185915) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2020_10_21_151520) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
