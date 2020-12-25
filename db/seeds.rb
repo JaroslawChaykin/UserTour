b65348047700c63792fcaad45ab61c26b07bade1
jaroslaw <jaroslaw.chaikin@gmail.com>
2020-12-26 01:05:17 +0300

    added db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,35 @@
-posts = [
-    {
-        author: "James",
-        title: "The Bad Seeds",
-        price: "1000",
-        timeBypass: "Nick Cave",
-        content: "Tracklist",
-        image: File.open(Rails.root.join('public', 'images', '1.png'))
-    },
-    {
-        author: "Brown",
-        title: "The Bad slsl",
-        price: "2600",
-        timeBypass: "Nick Cave Old Too",
-        content: "Some Old Tracklist",
-        image: File.open(Rails.root.join('public', 'images', '2.png'))
-    },
-    {
-        author: "Nick",
-        title: "The Bad slsl",
-        price: "4500",
-        timeBypass: "Nick Cave Old Too",
-        content: "Some Old Tracklist",
-        image: File.open(Rails.root.join('public', 'images', '3.png'))
-    }
-]
-
-posts.each do |post|
-  postik = Post.create(post)
-  puts "Created with id - #{ postik.id }"
-end
-
+# # frozen_string_literal: true
+#
+# posts = [
+#   {
+#     author: 'James',
+#     title: 'The Bad Seeds',
+#     price: '1000',
+#     timeBypass: 'Nick Cave',
+#     content: 'Tracklist',
+#     image: File.open(Rails.root.join('public', 'images', '1.png'))
+#   },
+#   {
+#     author: 'Brown',
+#     title: 'The Bad slsl',
+#     price: '2600',
+#     timeBypass: 'Nick Cave Old Too',
+#     content: 'Some Old Tracklist',
+#     image: File.open(Rails.root.join('public', 'images', '2.png'))
+#   },
+#   {
+#     author: 'Nick',
+#     title: 'The Bad slsl',
+#     price: '4500',
+#     timeBypass: 'Nick Cave Old Too',
+#     content: 'Some Old Tracklist',
+#     image: File.open(Rails.root.join('public', 'images', '3.png'))
+#   }
+# ]
+#
+# posts.each do |post|
+#   postik = Post.create(post)
+#   puts "Created with id - #{postik.id}"
+# end
+#
 
